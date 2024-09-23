e910525ed9958ae6aa5243dd6587967cc33077e8
lisonge <i@songe.li>
2024-09-23 17:28:03 +0800

    fix: rm host

--- src/main.ts
+++ src/main.ts
@@ -27,18 +27,25 @@ const httpsAgent = new https.Agent({
   lookup,
 });
 
+// 必须剔除 host 字段, 否则请求服务域名是错误的
+const removeHeaders = ['host', 'connection', 'referer'];
+
 const server = http.createServer(async (req, res) => {
-  const url = req.url || '/';
+  const url = (req.url || '/').trimEnd();
   const i = url.indexOf('/', 1);
   const targetUrl = url.substring(i + 1);
+  if (!(targetUrl.startsWith('https://') || targetUrl.startsWith('http://'))) {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
   record.address = url.substring(1, i);
   record.hostname = new URL(targetUrl).hostname;
   console.log(`${record.address} => ${targetUrl}`);
   const headers = Object.fromEntries(
-    Object.entries(req.headers).filter(([k, v]) => typeof v === 'string') as [
-      string,
-      string
-    ][]
+    Object.entries(req.headers).filter(
+      ([k, v]) => typeof v === 'string' && !removeHeaders.includes(k)
+    ) as [string, string][]
   );
   const agent = targetUrl.startsWith('https:') ? httpsAgent : httpAgent;
   const method = req.method;
